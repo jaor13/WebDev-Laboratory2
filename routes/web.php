6638ba4f27d6616f798b35ff69a7665321d074b0
jaor13 <jaor2022-6131-98317@bicol-u.edu.ph>
2024-09-06 11:24:16 +0800

    added group routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,46 +2,49 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index'); //named route that returns a view for the homepage
-})->name('homepage');
 
-Route::get('/about', function () {
-    return view('about'); //named route that returns a view for the about page
-})->name('about');
+    Route::get('/', function () {
+        return view('index'); //named route that returns a view for the homepage
+    })->name('homepage');
 
-Route::redirect('/home', '/'); //redirects /home to / (which is index, named as homepage) 
+    Route::redirect('/home', '/'); //redirects /home to / (which is index, named as homepage) 
 
-Route::get('/contact-us', function () {
-    return view('contact-us'); //named route that returns a view for the contact-us page
-})->name('contact-us');
+    Route::group(['prefix' => 'pages'], function () {
 
+        Route::get('/about', function () {
+            return view('about'); //named route that returns a view for the about page
+        })->name('about');
 
-/* route with required parameter
-Route::get('/user/{username}', function ($username) {
-    return view('index', ['username' => $username]);
-});
-*/
+        Route::get('/contact-us', function () {
+            return view('contact-us'); //named route that returns a view for the contact-us page
+        })->name('contact-us');
 
-/* route with optional parameter
-Route::get('/user/{username?}', function ($username = 'Guest') {
-    return view('welcome_user', ['username' => $username]);
-});
-*/
+        Route::get('/education-experience', function () {
+            return view('education-experience');
+        })->name('education-experience');//named route that returns a view for the education-experience page
 
-Route::get('/user/{username?}', function ($username = null) { //route with regular expression constraints
-    // Check if the username is valid (only alphabetic characters)
-    if (!preg_match('/^[a-zA-Z]+$/', $username)) {
-        $username = 'Guest';
-    }
-    return view('index', ['username' => $username]); //passes the $username variable to the view
-});
+        Route::get('/project-skills', function () {
+            return view('project-skills');//named route that returns a view for the project-skills page
+        })->name('project-skills');
 
+    });
 
-Route::get('/education-experience', function () {
-    return view('education-experience');
-})->name('education-experience');//named route that returns a view for the education-experience page
+     /* route with required parameter
+    Route::get('/user/{username}', function ($username) {
+        return view('index', ['username' => $username]);
+    });
+    */
 
-Route::get('/project-skills', function () {
-    return view('project-skills');//named route that returns a view for the project-skills page
-})->name('project-skills');
+    /* route with optional parameter
+    Route::get('/user/{username?}', function ($username = 'Guest') {
+        return view('welcome_user', ['username' => $username]);
+    });
+    */
+
+    Route::get('/user/{username?}', function ($username = null) { //route with regular expression constraints
+        // Check if the username is valid (only alphabetic characters)
+        if (!preg_match('/^[a-zA-Z]+$/', $username)) {
+            $username = 'Guest';
+        }
+        return view('index', ['username' => $username]); //passes the $username variable to the view
+    });
